c9697b75bd099635df535529d0f142e372fdb7bf
huangguorui <2315684336@qq.com>
2021-05-19 23:14:52 +0800

    添加判断，只有输入了名称才进行模糊匹配，解决了在没有输入名称列表显示为空的bug

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.service;
 
+import com.github.pagehelper.PageHelper;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
@@ -8,6 +9,7 @@ import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -20,12 +22,16 @@ public class EbookService {
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req) {
+
+
         //Example 相当于where条件 不传值可以设置为null或者new EbookExample()
         //ctrl+alt+v 快速生成变量
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         //左匹配和右匹配
-        criteria.andNameLike("%" + req.getName() + "%");
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
 
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
