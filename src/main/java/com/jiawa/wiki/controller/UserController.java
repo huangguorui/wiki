ef3b03a24895e9f34398eb4de0c0235c418f9413
huangguorui <2315684336@qq.com>
2021-05-22 14:53:52 +0800

    9-9 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/jiawa/wiki/controller/UserController.java
+++ src/main/java/com/jiawa/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jiawa.wiki.req.UserLoginReq;
 import com.jiawa.wiki.req.UserQueryReq;
 import com.jiawa.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,30 @@ import com.jiawa.wiki.resp.UserLoginResp;
 import com.jiawa.wiki.resp.UserQueryResp;
 import com.jiawa.wiki.resp.PageResp;
 import com.jiawa.wiki.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
+import sun.rmi.runtime.Log;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+    
+    
     @Resource
     private UserService userService;
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req){
@@ -56,6 +69,12 @@ public class UserController {
         //ctrl+alt+v 快速生成一个变量
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+        //这里没有用雪花算法，使用的是uuid
+        String token = UUID.randomUUID().toString();
+        //大写的Long可以直接toString  logi
+        LOG.info("生成单点登录token，{}，并放入redis中",token);
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp),3600*24, TimeUnit.SECONDS);
+        userLoginResp.setToken(token);
         resp.setContent(userLoginResp);
         return resp;
     }
